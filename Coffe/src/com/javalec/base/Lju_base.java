7fc3c1364d31c9a9fb052254feac508f765374b9
이종욱 <gosirock@naver.com>
2023-05-11 14:36:23 +0900

    Start

---
+++ Coffe/src/com/javalec/base/Lju_base.java
@@ -0,0 +1,125 @@
+package com.javalec.base;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;;
+
+public class Lju_base extends JFrame {
+
+	private JPanel contentPane;
+	private JPanel panel;
+	private JButton btnHome;
+	private JButton btnMenu;
+	private JButton btnOrder;
+	private JButton btnReview;
+	private JLabel lblNewLabel_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Lju_base frame = new Lju_base();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Lju_base() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 390, 872);							// 백그라운드 프레임 패널 사이즈
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(248, 227, 182));	// 백그라운드 프레임 패널 색
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		contentPane.add(getPanel());
+		contentPane.add(getLblNewLabel_1());
+	}
+	private JPanel getPanel() {
+		if (panel == null) {
+			panel = new JPanel();
+			panel.setBackground(new Color(131, 77, 30));		// 하단 버튼바 패널 색상
+			panel.setBounds(0, 757, 390, 87);					// 하단 버튼바 패널 위치,사이즈
+			panel.setLayout(new GridLayout(1, 0, 0, 0));
+			panel.add(getBtnHome());
+			panel.add(getBtnMenu());
+			panel.add(getBtnOrder());
+			panel.add(getBtnReview());
+		}
+		return panel;
+	}
+	private JButton getBtnHome() {
+		if (btnHome == null) {
+			btnHome = new JButton("");
+			btnHome.setIcon(new ImageIcon(Lju_base.class.getResource("/com/javalec/image/btnHome.png")));
+			btnHome.setFocusPainted(false);					// 버튼 포커스라인 없애기
+			btnHome.setBorderPainted(false);
+			btnHome.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+				}
+			});
+		}
+		return btnHome;
+	}
+	private JButton getBtnMenu() {
+		if (btnMenu == null) {
+			btnMenu = new JButton("");
+			btnMenu.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+				}
+			});
+			btnMenu.setIcon(new ImageIcon(Lju_base.class.getResource("/com/javalec/image/btnMenu.png")));
+			btnMenu.setFocusPainted(false);
+			btnMenu.setBorderPainted(false);
+		}
+		return btnMenu;
+	}
+	private JButton getBtnOrder() {
+		if (btnOrder == null) {
+			btnOrder = new JButton("");
+			btnOrder.setIcon(new ImageIcon(Lju_base.class.getResource("/com/javalec/image/btnOrder.png")));
+			btnOrder.setFocusPainted(false);
+			btnOrder.setBorderPainted(false);
+		}
+		return btnOrder;
+	}
+	private JButton getBtnReview() {
+		if (btnReview == null) {
+			btnReview = new JButton("");
+			btnReview.setIcon(new ImageIcon(Lju_base.class.getResource("/com/javalec/image/btnReview.png")));
+			btnReview.setFocusPainted(false);
+			btnReview.setBorderPainted(false);
+		}
+		return btnReview;
+	}
+	
+	private JLabel getLblNewLabel_1() {
+		if (lblNewLabel_1 == null) {
+			lblNewLabel_1 = new JLabel("");
+			lblNewLabel_1.setIcon(new ImageIcon(Lju_Menu01.class.getResource("/com/javalec/image/wifi.png")));
+			lblNewLabel_1.setBounds(303, 15, 65, 18);
+		}
+		return lblNewLabel_1;
+	}
+	
+}
