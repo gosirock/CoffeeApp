13ab1d9f633550097d87aed5a95f3e1bf145c08b
wookhyunKim <kwh1041@naver.com>
2023-05-16 13:54:33 +0900

    최신

--- Coffe/src/com/javalec/util/ShareVar.java
+++ Coffe/src/com/javalec/util/ShareVar.java
@@ -4,13 +4,13 @@ public class ShareVar {
 	
 	// dB변경되면 여기서 변경시켜주면 됨
 	
-	public static final String DBName = "jdbc:mysql://127.0.0.1/cafe_app?serverTimezone=UTC&characterEncoding=utf8&useSSL=FALSE";
-	public static final String DBUser = "root";
+	public static final String DBName = "jdbc:mysql://1.234.95.198/cafe_app?serverTimezone=UTC&characterEncoding=utf8&useSSL=FALSE";
+	public static final String DBUser = "root5";
 	public static final String DBPass = "qwer1234";
 	public static int filename = 0;  // dB에서의 blob을 file로 바꾸기 위해 해주는 작업
 	
 	
-	public static String loginUserId = "gosi";
+	public static String loginUserId = "";
 	public static String ItemId = "";
 	
 	
